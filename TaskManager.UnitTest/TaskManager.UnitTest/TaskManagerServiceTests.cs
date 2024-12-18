5d2fd3c610f30ba228144e3b62a72468bc736d50
Ángel <angel.ortega.alfaro@gmail.com>
2024-12-18 16:59:43 +0100

    Add more unit test

--- TaskManager.UnitTest/TaskManager.UnitTest/TaskManagerServiceTests.cs
+++ TaskManager.UnitTest/TaskManager.UnitTest/TaskManagerServiceTests.cs
@@ -51,10 +51,13 @@ namespace TaskManager.UnitTest.Service
             Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
             Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
             var request = new TaskMessage { Name = "Test Task", Description = "Test Description", Status = Status.Open };
+            var expectedId = 1;
 
             fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
             fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
 
+            _mockTaskRepository.Setup(repo => repo.CreateTaskAsync(It.IsAny<TaskElement>()))
+                    .ReturnsAsync(expectedId);
             _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()))
                                 .Throws(new Exception("Error sending message"));
 
@@ -111,6 +114,7 @@ namespace TaskManager.UnitTest.Service
             fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
 
             _mockTaskRepository.Setup(repo => repo.GetTaskByIdAsync(It.IsAny<int>())).ReturnsAsync(element);
+            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()));
 
             TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
 
@@ -138,8 +142,9 @@ namespace TaskManager.UnitTest.Service
             fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
             fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
 
-            _mockTaskRepository.Setup(db => db.GetTaskByIdAsync(It.IsAny<int>()))
-                                .ThrowsAsync(new Exception("Error sending message"));
+            _mockTaskRepository.Setup(repo => repo.GetTaskByIdAsync(It.IsAny<int>())).ReturnsAsync(element);
+            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()))
+                .Throws(new Exception("Error sending message"));
 
             TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
 
@@ -167,9 +172,8 @@ namespace TaskManager.UnitTest.Service
             fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
             fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
 
-            _mockTaskRepository.Setup(repo => repo.GetTaskByIdAsync(It.IsAny<int>())).ReturnsAsync(element);
-            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()))
-                    .Throws(new DbUpdateException("Error sending message"));
+            _mockTaskRepository.Setup(repo => repo.GetTaskByIdAsync(It.IsAny<int>()))
+                .ThrowsAsync(new DbUpdateException("Error sending message"));
 
             TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
 
@@ -178,8 +182,8 @@ namespace TaskManager.UnitTest.Service
 
             // Assert
 
-            _mockTaskRepository.Verify(repo => repo.GetTaskByIdAsync(It.IsAny<int>()), Times.Once);
-            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Once);
+            _mockTaskRepository.Verify(repo => repo.GetTaskByIdAsync(It.IsAny<int>()), Times.Never);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Never);
         }
 
         [Fact]
@@ -197,6 +201,7 @@ namespace TaskManager.UnitTest.Service
             fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
 
             _mockTaskRepository.Setup(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>())).ReturnsAsync(new TaskResponseEmpty());
+            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()));
 
             TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
 
@@ -212,7 +217,130 @@ namespace TaskManager.UnitTest.Service
         [Fact]
         public async Task UpdateTask_Error_RabbitMQException()
         {
+            //arrange
+            var fixture = new Fixture();
+            Mock<ILogger<TaskManagerService>> _mockLogger = new Mock<ILogger<TaskManagerService>>();
+            Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
+            Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
+            TaskMessageId request = new TaskMessageId { Id = 1 };
+            TaskElement element = new TaskElement { Name = "Test Task", Description = "Test Description", status = Status.Open.ToString() };
 
+            fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
+            fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
+
+            _mockTaskRepository.Setup(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>())).ReturnsAsync(new TaskResponseEmpty());
+            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()))
+                                .Throws(new Exception("Error sending message"));
+
+            TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
+
+            await Assert.ThrowsAsync<Exception>(() => _service.UpdateTask(request, null));
+
+            _mockTaskRepository.Verify(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>()), Times.Once);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task UpdateTask_Error_DbException()
+        {
+            //arrange
+            var fixture = new Fixture();
+            Mock<ILogger<TaskManagerService>> _mockLogger = new Mock<ILogger<TaskManagerService>>();
+            Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
+            Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
+            TaskMessageId request = new TaskMessageId { Id = 1 };
+            TaskElement element = new TaskElement { Name = "Test Task", Description = "Test Description", status = Status.Open.ToString() };
+
+            fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
+            fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
+
+            _mockTaskRepository.Setup(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>()))
+                .ThrowsAsync(new DbUpdateException("Error updating database"));
+
+            TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
+
+            await Assert.ThrowsAsync<DbUpdateException>(() => _service.UpdateTask(request, null));
+
+            _mockTaskRepository.Verify(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>()), Times.Once);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task DeleteTask_Successful()
+        {
+            //arrange
+            var fixture = new Fixture();
+            Mock<ILogger<TaskManagerService>> _mockLogger = new Mock<ILogger<TaskManagerService>>();
+            Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
+            Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
+            TaskIdMessage request = new TaskIdMessage { Id = 1 };
+            TaskElement element = new TaskElement { Name = "Test Task", Description = "Test Description", status = Status.Open.ToString() };
+
+            fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
+            fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
+
+            _mockTaskRepository.Setup(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>())).ReturnsAsync(new TaskResponseEmpty());
+
+            TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
+
+            // Act
+            var result = await _service.DeleteTask(request, null);
+
+            // Assert
+            Assert.NotNull(result);
+            _mockTaskRepository.Verify(repo => repo.DeleteTaskAsync(It.IsAny<int>()), Times.Once);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteTask_Error_RabbitMQException()
+        {
+            //arrange
+            var fixture = new Fixture();
+            Mock<ILogger<TaskManagerService>> _mockLogger = new Mock<ILogger<TaskManagerService>>();
+            Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
+            Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
+            TaskMessageId request = new TaskMessageId { Id = 1 };
+            TaskElement element = new TaskElement { Name = "Test Task", Description = "Test Description", status = Status.Open.ToString() };
+
+            fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
+            fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
+
+            _mockTaskRepository.Setup(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>())).ReturnsAsync(new TaskResponseEmpty());
+            _mockRabbitMQService.Setup(rabbit => rabbit.SendMessage(It.IsAny<string>()))
+                                .Throws(new Exception("Error sending message"));
+
+            TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
+
+            await Assert.ThrowsAsync<Exception>(() => _service.UpdateTask(request, null));
+
+            _mockTaskRepository.Verify(repo => repo.UpdateTaskAsync(It.IsAny<TaskElement>()), Times.Once);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task DeleteTask_Error_DbException()
+        {
+            //arrange
+            var fixture = new Fixture();
+            Mock<ILogger<TaskManagerService>> _mockLogger = new Mock<ILogger<TaskManagerService>>();
+            Mock<ITaskRepository> _mockTaskRepository = new Mock<ITaskRepository>();
+            Mock<IRabbitMQService> _mockRabbitMQService = new Mock<IRabbitMQService>();
+            TaskIdMessage request = new TaskIdMessage { Id = 1 };
+            TaskElement element = new TaskElement { Name = "Test Task", Description = "Test Description", status = Status.Open.ToString() };
+
+            fixture.Customize<ILogger<RabbitMQService>>(c => c.FromFactory(() => Mock.Of<ILogger<RabbitMQService>>()));
+            fixture.Customize<IOptions<RabbitMQConfiguration>>(c => c.FromFactory(() => Mock.Of<IOptions<RabbitMQConfiguration>>()));
+
+            _mockTaskRepository.Setup(repo => repo.DeleteTaskAsync(It.IsAny<int>()))
+                .ThrowsAsync(new DbUpdateException("Error updating database"));
+
+            TaskManagerService _service = new TaskManagerService(_mockLogger.Object, _mockTaskRepository.Object, _mockRabbitMQService.Object);
+
+            await Assert.ThrowsAsync<DbUpdateException>(() => _service.DeleteTask(request, null));
+
+            _mockTaskRepository.Verify(repo => repo.DeleteTaskAsync(It.IsAny<int>()), Times.Once);
+            _mockRabbitMQService.Verify(rabbit => rabbit.SendMessage(It.IsAny<string>()), Times.Never);
         }
     }
 }
